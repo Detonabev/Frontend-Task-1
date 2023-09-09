506fe476404d26d16c3ac6f8e2f4f5980e0c90c9
Dara <Detonabev@gmail.com>
2023-09-09 14:20:21 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let day = document.getElementById('day')
+let time = document.getElementById('current-time')
+
+let d = new Date();
+let name = d.getDay()
+
+switch (name){
+    case 0:
+        name = 'Sunday';
+        break;
+    case 1:
+        name = 'Monday';
+        break;
+    case 2:
+        name = 'Tuesday';
+        break;
+    case 3:
+        name = 'Wednesday';
+        break;
+    case 4:
+        name = 'Thursday';
+        break;
+    case 5:
+        name = 'Friday';
+        break;
+    case 6:
+        name = 'Saturday';
+        break;
+    
+}
+
+
+day.innerHTML = name;
+time.innerHTML = d.toLocaleTimeString();
+// setInterval(()=>{
+    
+
+// },1000)
